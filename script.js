48aa37f2898dbb54b97e447687ecf09cfdb97b2c
Jake Meyers <jakejake92@yahoo.com>
2020-11-12 16:51:08 -0700

    API call working and creating elements

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const APP_ID = "511a5efa-6ff4-4676-925e-82528aa15538";
 const API_KEY = "Bn1HVpG92uAJyIirwt0MaXniuqAJpLyK";
 const articlesDiv = document.getElementById("articles");
 
-fetch(
-	`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`
-);
-
 async function getArticles() {
-	const response = await fetch(
-		`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY`,
-	);
+	const result = await fetch(
+		`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`,
+		{
+			method: "GET",
+		}
+	)
+		.then((res) => res.json())
+		.then((data) => {
+			data = data.results;
+			console.log(data);
+			data.forEach((element) => {
+				const title = document.createElement("h2");
+				const abstract = document.createElement("p");
+				title.innerHTML = element.title;
+				abstract.innerHTML = element.abstract;
+				articlesDiv.appendChild(abstract);
+			});
+		});
 }
+getArticles();
